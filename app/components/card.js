8325086637ecb5d1999dfccb8ea34db14e8df640
omsa0 <theos2014.os@gmail.com>
2024-11-04 21:48:37 -0600

    session 18

---
+++ app/components/card.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class CardComponent extends Component {
+  @action
+  flipCard(event) {
+    const element = event.target.closest('.card');
+    // console.log(element);
+    const question = element.querySelector('.question');
+    const answer = element.querySelector('.answer');
+    question.classList.toggle('hidden');
+    answer.classList.toggle('hidden');
+  }
+  
+}
