1dadc4c0e97b3534d1a86be909e3d4bbd74eab41
omsa0 <theos2014.os@gmail.com>
2024-11-10 13:48:27 -0600

    navbar fix

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -5,13 +5,11 @@ export default class NavbarComponent extends Component {
   // Navbar Script for Burger Menu
   @action
   toggleBurger(event) {
-    const el = event.target;
-    // console.log(el);
+    const el = event.target.closest('.navbar-burger');
 
     // Get the target from the "data-target" attribute
     const target = el.dataset.target;
     const $target = document.getElementById(target);
-    // console.log($target);
 
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     el.classList.toggle('is-active');
