711eb104d642eda83903202cd794c57867f67b1c
omsa0 <theos2014.os@gmail.com>
2024-11-13 17:25:40 -0600

    replace everything with auth service

--- app/controllers/firebasetesting.js
+++ app/controllers/firebasetesting.js
@@ -1,26 +1,24 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
-import { signInWithPopup, GoogleAuthProvider, getAuth, signOut, onAuthStateChanged, authStateReady } from 'firebase/auth';
 
 export default class FirebasetestingController extends Controller {
-    @service firebase;
+  @service auth;
 
-    @action 
-    test() {
-        // Everything is based on the auth object
-        const auth = getAuth(this.firebase.app);
-        console.log(auth.currentUser);
+  @action
+  async test() {
+    await this.auth.init();
 
-        if (!auth.currentUser) {
-            // Sign In
-            const provider = new GoogleAuthProvider();
-            signInWithPopup(auth, provider)
-            console.log("Signed In");
-        } else {
-            // Sign Out
-            signOut(auth);
-            console.log("Signed Out");
-        }
+    if (!this.auth.user) {
+      // Sign In
+      this.auth.sign_in_with_popup();
+      console.log(this.auth.user);
+      console.log("Signed In");
+    } else {
+      // Sign Out
+      this.auth.sign_out();
+      console.log(this.auth.user);
+      console.log("Signed Out");
     }
+  }
 }
