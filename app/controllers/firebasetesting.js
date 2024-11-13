b00aecd9b6e6909dc9307a0eb0bae52f8d84e7d0
omsa0 <theos2014.os@gmail.com>
2024-11-13 17:00:55 -0600

    basic auth login/logout

--- app/controllers/firebasetesting.js
+++ app/controllers/firebasetesting.js
@@ -1,12 +1,27 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { signInWithPopup, GoogleAuthProvider, getAuth, signOut, onAuthStateChanged, authStateReady } from 'firebase/auth';
 
 export default class FirebasetestingController extends Controller {
     @service firebase;
 
     @action 
     test() {
-        console.log(this.firebase.db);
+        // Everything is based on the auth object
+        const auth = getAuth(this.firebase.app);
+        console.log(auth.currentUser);
+
+        if (!auth.currentUser) {
+            // Sign In
+            const provider = new GoogleAuthProvider();
+            signInWithPopup(auth, provider)
+            console.log("Signed In");
+        } else {
+            // Sign Out
+            signOut(auth);
+            console.log("Signed Out");
+        }
+
     }
 }
