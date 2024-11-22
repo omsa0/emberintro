aae2532a5019263427409e6fe1a9baa95587b9a6
omsa0 <theos2014.os@gmail.com>
2024-11-22 00:41:27 -0600

    new api for transitioning

--- app/controllers/flashcards.js
+++ app/controllers/flashcards.js
@@ -4,4 +4,21 @@ import { service } from '@ember/service';
 export default class FlashcardsController extends Controller {
   @service auth;
   @service cards;
+  @service router;
+
+  // another way to handle the change but instead in the controller
+  // constructor() {
+  //   super(...arguments);
+
+  //   // when leaving the page, unsubscribe from changes.
+  //   this.router.on('routeWillChange', (transition) => {
+  //     console.log(`Leaving route: ${this.router.currentRouteName}`);
+  //     console.log(`Going to route: ${transition.to.name}`);
+  //     console.log(transition)
+  //     if (!transition.to.find((route) => route.name.startsWith('flashcards'))) {
+  //       console.log('unsubscribing from cards');
+  //       this.cards.unsubscribe();
+  //     }
+  //   });
+  // }
 }
--- app/routes/flashcards.js
+++ app/routes/flashcards.js
@@ -10,17 +10,40 @@ export default class FlashcardsRoute extends Route {
     await this.cards.fetchCards();
   }
 
-  actions = {
-    willTransition(transition) {
+  // another way to handle the change but instead in the route
+  // this is the new api
+  // I think it makes more sense to handle this in the route
+  // additionally, the new api 'routeWillChange' also triggers for
+  // loading and error substates which can be nice but we have to use
+  // startsWith to account for it, which is a bit of a pain
+  constructor() {
+    super(...arguments);
+
+    // when leaving the page, unsubscribe from changes.
+    this.router.on('routeWillChange', (transition) => {
       console.log(`Leaving route: ${this.router.currentRouteName}`);
       console.log(`Going to route: ${transition.to.name}`);
-
-      if (transition.to.name !== 'flashcards') {
+      console.log(transition)
+      if (!transition.to.find((route) => route.name.startsWith('flashcards'))) {
+        console.log('unsubscribing from cards');
         this.cards.unsubscribe();
-        console.log('Unsubscribed from cards');
       }
-    },
-  };
+    });
+  }
+
+  // another way to handle the change but instead in the route
+  // this is the old api
+  // actions = {
+  //   willTransition(transition) {
+  //     console.log(`Leaving route: ${this.router.currentRouteName}`);
+  //     console.log(`Going to route: ${transition.to.name}`);
+
+  //     if (transition.to.name !== 'flashcards') {
+  //       this.cards.unsubscribe();
+  //       console.log('Unsubscribed from cards');
+  //     }
+  //   },
+  // };
 
   // model() {
   //   throw new Error('OH NO');
